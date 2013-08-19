33d1c903571cdfb0b712bdb77e77665f2759bdd6
Tim Cameron Ryan <id@timryan.org>
2013-08-19 15:07:37 -0400

    Completely use I2C async API

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var tm = process.binding('tm');
-var EventEmitter = require('tessel').EventEmitter;
-
 // address bit is 111xy11 where x is controlled by GPIO2 and y is controlled by GPIO1
 // by default x and y will be 0
 // used http://www.nxp.com/documents/data_sheet/PCA9685.pdf as a reference
+
+var tessel = require('tessel');
+var EventEmitter = require('tessel').EventEmitter;
+
+//
+// I2C Configuration
+//
 
 var I2C_ADDRESS = 0x73;
 var LED0_ON_L = 0x06;
@@ -14,58 +18,21 @@ var MAX = 4096;
 var MODE1 = 0x0;
 var PRE_SCALE = 0xFE;
 
-
-// var i2c = tessel.i2c(1);
-
-// i2c = function ()
-
-var i2cidx = tm.I2C_1;
-
-i2c = {
-  request: function (addr, data, size, fn) {
-    var ret = tm.i2c_master_request_blocking(i2cidx, addr, data, size);
-    setImmediate(function () {
-      fn && fn(null, ret);
-    });
-  },
-  send: function (addr, data, fn) {
-    tm.i2c_master_send_blocking(i2cidx, addr, data);
-    setImmediate(function () {
-      fn && fn(null);
-    });
-  },
-  receive: function (addr, size, fn) {
-    var ret = tm.i2c_master_receive_blocking(i2cidx, addr, size);
-    setImmediate(function () {
-      fn && fn(null, ret);
-    });
-  }
-};
+var i2c = tessel.i2c(I2C_ADDRESS);
 
 function readRegister (register, next) {
-  i2c.request(I2C_ADDRESS, [register], 1, function (err, data) {
+  i2c.request([register], 1, function (err, data) {
     next(err, data[0]);
   });
 }
 
 function writeRegister (register, data, next) {
-  i2c.send(I2C_ADDRESS, [register, data], next)
+  i2c.send([register, data], next)
 }
 
-// TODO: fix this
-// function read_servo(servo){
-//   var on_low = read_register(LED0_ON_L+(servo-1)*4);
-//   var on_high = read_register(LED0_ON_H+(servo-1)*4)>>8;
-//   console.log("on_low: ", on_low, " on_high: ", on_high);
-//   var on = on_low + on_high;
-//   var off_low = read_register(LED0_OFF_L+(servo-1)*4);
-//   var off_high = read_register(LED0_OFF_H+(servo-1)*4)>>8;
-//   console.log("off_low: ", off_low, " off_high: ", off_high);
-
-//   var off = off_low + off_high;
-
-//   console.log("Servo: ", servo, " on: ", on, " off: ", off);
-// }
+/**
+ * Frequency
+ */
 
 // sets the driver frequency. freq has units of Hz
 function setFrequency (freq, next) {
@@ -99,19 +66,17 @@ function Servo (idx, low, high) {
 Servo.prototype = new EventEmitter();
 
 // 0...180
-Servo.prototype.move = function (val) {
-  this.setPWM(((val/180) * (this.high - this.low)) + this.low);
-  
-  // TODO async I2C
-  // var servo = this;
-  // setImmediate(function () {
-  //   servo.emit('move');
-  // })
+Servo.prototype.move = function (val, next) {
+  var servo = this;
+  this.setPWM(((val/180) * (this.high - this.low)) + this.low, function () {
+    // this.emit('move'); TODO
+    next && next();
+  });
 };
 
 // servo: 1... 16
 // on: 1...100% of time that the servo is on
-Servo.prototype.setPWM = function (on) {
+Servo.prototype.setPWM = function (on, next) {
   var convert_on = 0;
   var convert_off = Math.floor(MAX/100*on);
 
@@ -119,18 +84,15 @@ Servo.prototype.setPWM = function (on) {
   writeRegister(LED0_ON_L+(this.idx-1)*4, convert_on);
   writeRegister(LED0_ON_H+(this.idx-1)*4, convert_on>>8);
   writeRegister(LED0_OFF_L+(this.idx-1)*4, convert_off);
-  writeRegister(LED0_OFF_H+(this.idx-1)*4, convert_off>>8);
+  writeRegister(LED0_OFF_H+(this.idx-1)*4, convert_off>>8, next);
 }
 
-exports.port = function () {
+function port (id) {
   return {
     connect: function (idx, low, high) {
+      i2c.initialize();
+
       var servo = new Servo(idx, low, high);
-
-      tm.i2c_initialize(tm.I2C_1);
-      tm.i2c_master_enable(tm.I2C_1);
-      console.log("Starting up PCA9685...");
-
       setFrequency(50, function () {
         servo.emit('connected');
       });
@@ -139,6 +101,30 @@ exports.port = function () {
   }
 }
 
-exports.connect = connect;
+//
+// Reading
+//
+
+// TODO: fix this
+// function read_servo(servo){
+//   var on_low = read_register(LED0_ON_L+(servo-1)*4);
+//   var on_high = read_register(LED0_ON_H+(servo-1)*4)>>8;
+//   console.log("on_low: ", on_low, " on_high: ", on_high);
+//   var on = on_low + on_high;
+//   var off_low = read_register(LED0_OFF_L+(servo-1)*4);
+//   var off_high = read_register(LED0_OFF_H+(servo-1)*4)>>8;
+//   console.log("off_low: ", off_low, " off_high: ", off_high);
+
+//   var off = off_low + off_high;
+
+//   console.log("Servo: ", servo, " on: ", on, " off: ", off);
+// }
+
+
+//
+// Public API
+//
+
+exports.port = port;
 exports.setFrequency = setFrequency;
 exports.Servo = Servo;
