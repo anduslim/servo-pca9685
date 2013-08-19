44daa3c68581ab9b2cdd4aea1e79190dfcea4048
Tim Cameron Ryan <id@timryan.org>
2013-08-19 15:08:54 -0400

    Update actuator cod

--- examples/sweep.js
+++ examples/sweep.js
@@ -1,10 +1,12 @@
 var servo = require('../');
 
 var actuator = servo.port('A').connect(1);
-actuator.move(0);
-actuator.once('move', function () {
-  actuator.move(90);
+actuator.on('connected', function () {
+  actuator.move(0);
   actuator.once('move', function () {
-    actuator.move(180);
-  });
-})
+    actuator.move(90);
+    actuator.once('move', function () {
+      actuator.move(180);
+    });
+  })
+});
